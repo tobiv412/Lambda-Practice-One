39e6bb5b42cd33ab06d9f48e26c6ad5ffee5dc81
tobiv412 <victor.tobi@cloudreach.com>
2021-12-02 13:48:35 +0000

    lambda function

--- lambda_function.tf
+++ lambda_function.tf
@@ -1,5 +1,5 @@
 resource "aws_lambda_function" "pet_lambda" {
-  filename      = "${path.module}/pet_info.zip"
+  filename      = "${path.module}.pet_info.output_path"
   function_name = "pet_info"
   role          = aws_iam_role.lambda_role.arn
   handler       = "pet_info"
@@ -7,9 +7,9 @@ resource "aws_lambda_function" "pet_lambda" {
 
   #source_code_hash = filebase64sha256("lambda_function_payload.zip")
 
-source_code_hash = data.archive_file.pet_script.output_base64sha256
+source_code_hash = data.archive_file.pet_info.output_base64sha256
 
-  runtime = "python3.8.x"
+  runtime = "python3.8"
 
  
 }
