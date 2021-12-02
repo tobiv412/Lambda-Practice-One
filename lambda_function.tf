22ea3961a49b7b30ed75509bf1b01bfb4fde379d
tobiv412 <victor.tobi@cloudreach.com>
2021-12-02 10:15:12 +0000

    update lambda function

--- lambda_function.tf
+++ lambda_function.tf
@@ -1,13 +1,15 @@
-resource "aws_lambda_function" "test_lambda" {
-  filename      = "boto3.zip"
-  function_name = "lambda_function_name"
+resource "aws_lambda_function" "pet_lambda" {
+  filename      = "${path.module}/pet_info.zip"
+  function_name = "pet_info"
   role          = aws_iam_role.lambda_role.arn
-  handler       = "boto3.food"
+  handler       = "pet_info"
 
 
   #source_code_hash = filebase64sha256("lambda_function_payload.zip")
 
-  runtime = "python3.10.x"
+source_code_hash = data.archive_file.pet_script.output_base64sha256
+
+  runtime = "python3.8.x"
 
  
 }
